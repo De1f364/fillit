d3e6345d86c6fd5a36dfedb6432163b14266dffa
Roxie Donnor <rdonnor@mi-u3.21-school.ru>
2019-05-27 22:00:09 +0300

    modified

--- src/main.c
+++ src/main.c
@@ -6,8 +6,22 @@
 /*   By: rdonnor <rdonnor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 21:37:01 by rdonnor           #+#    #+#             */
-/*   Updated: 2019/05/27 21:37:01 by rdonnor          ###   ########.fr       */
+/*   Updated: 2019/05/27 21:53:44 by rdonnor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fillit.h"
+
+int main(int argc, char **argv)
+{
+	int fd;
+
+	if (argc == 2)
+	{
+		fd = open(argv[1], O_RDONLY);
+	}
+	else
+		ft_putstr("usage: you need to put 1 file as parametr");
+
+	return (0);
+}
