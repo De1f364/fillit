679ba13f5db694914c64a457b1734d36ea69659b
= <=>
2019-05-30 19:12:55 +0300

    make_str_map

--- src/main.c
+++ src/main.c
@@ -1,11 +1,27 @@
 #include "../includes/fillit.h"
 
+char    *make_str_map(char **str)
+{
+    static char *tmp;
+    char        arr[26];
+    int         i;
+
+    i = 0;
+    if (ft_strlen(&tmp) != 16)
+        tmp = ft_strjoin(tmp, *str);
+    arr[i] = tmp;
+
+    return (tmp);
+}
+
+
 int main(int argc, char **argv)
 {
 	int fd;
 	int  ret;
 	char *str;
 	int count;
+	char *tmp;
 
 	count = 0;
 	if (argc == 2)
@@ -13,6 +29,7 @@ int main(int argc, char **argv)
 		fd = open(argv[1], O_RDONLY);
 		while ((ret= get_next_line(fd, &str)) > 0)
 		{
+            tmp = make_str_map(&str);
 			count++;
 			if (ft_strlen(str) != 4)
 			{
